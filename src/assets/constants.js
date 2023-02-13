99ea60efae00a8ce09c070842ae633050a0c41fd
MikhaelVinicius <mickaelsoel@gmail.com>
2023-02-13 03:38:46 -0300

    Siderbar implementado

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -18,8 +18,8 @@ export const genres = [
 ];
 
 export const links = [
-  { name: 'Discover', to: '/', icon: HiOutlineHome },
-  { name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
-  { name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
-  { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
+  { name: 'Descubrar', to: '/', icon: HiOutlineHome },
+  { name: 'Ao seu redor', to: '/around-you', icon: HiOutlinePhotograph },
+  { name: 'Top Artistas', to: '/top-artists', icon: HiOutlineUserGroup },
+  { name: 'Top paradas', to: '/top-charts', icon: HiOutlineHashtag },
 ];
