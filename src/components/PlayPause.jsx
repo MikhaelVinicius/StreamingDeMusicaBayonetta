932a6ea6ceaa61946d85a99874de617d42326a19
MikhaelVinicius <mickaelsoel@gmail.com>
2023-02-13 01:43:59 -0300

    Componentes dos containers de músicas criados!

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,15 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import {FaPauseCircle, FaPlayCircle} from 'react-icons/fa';
+
+
+const PlayPause = ({isPlaying, activeSong,song,handLePause,handLePlay}) => (isPlaying && activeSong?.title === song.title?(
+  <FaPauseCircle
+  size={35}
+  className="text-gray-300"
+  onClick={handLePause}/>
+)
+: (<FaPlayCircle
+  size={35}
+  className="text-gray-300"
+  onClick={handLePlay}/>))
 
 export default PlayPause;
