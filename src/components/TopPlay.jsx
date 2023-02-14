637d442099bc3f8f7455fd31f9155cb4f0e68102
MikhaelVinicius <mickaelsoel@gmail.com>
2023-02-14 07:15:44 -0300

    Aplicado funcionalidade do Top Artistas e mudança no tamanho do sidebar

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,10 +11,30 @@ import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 import { data } from "autoprefixer";
 import { TopCharts } from "../pages";
 
-const TopChartCard = ({song, i}) => (
+const TopChartCard = ({song, i, isPlaying,activeSong,handlePauseClick,handlePlayClick}) => (
    <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-    {song.title}
-    
+    <h3 className="fot-bold text-base text-white mr-3">{i +1}.</h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+      <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/songs/${song.key}`} >
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+          </Link>
+          <Link to={`/artists/${song?.artists[0].adamid}`} >
+          <p className="text-ase text-gray-300 m-1">{song?.subtitle}</p>
+          </Link>  
+
+      </div>
+
+    </div>
+    <PlayPause 
+    isPlaying={isPlaying}
+    activeSong={activeSong}
+    song={song}
+    handLePause={handlePauseClick}
+    handLePlay={handlePlayClick}
+
+    />
      </div>
   
 
@@ -39,18 +59,18 @@ const topPlays = data?.slice(0,5);
 const handlePauseClick =() => {
   dispatch(playPause(false));
 };
-const handlePlayClick = () => {
+const handlePlayClick = (song, i) => {
 dispatch(setActiveSong({song, data, i}));
 dispatch(playPause(true));
 };
 
 return (
   <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6
-  flex-1 xl:mx-w-[500px] max-w-full flex flex-col">
+  flex-1 xl:max-w-[500px] max-w-full flex flex-col">
     <div className="w-full flex flex-col">
       <div className="flex flex-row justify-between items-center">
         <h2 className="text-white font-bold text-2xl
-        ">Top Paradas</h2>
+        ">Em alta</h2>
         <Link to="/top-charts">
           <p className="text-gray-300 text-base cursor-pointer">Veja mais</p>
         </Link>
@@ -60,7 +80,13 @@ return (
             <TopChartCard 
             key = {song.key}
             song={song}
-            i={i}/>
+            i={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={()=> handlePlayClick(song,i)}
+            
+            />
           ))}
         </div>
       </div>
@@ -73,6 +99,28 @@ return (
           <p className="text-gray-300 text-base cursor-pointer">Veja mais</p>
         </Link>
         </div>
+        <Swiper slidesPerView="auto"
+        spaceBetween={15}
+        freeMode
+        centeredSlides
+        centeredSlidesBounds
+        modules={[FreeMode]}
+        className="mt-4"
+        >
+          {topPlays?.map((song, i)=> (
+            <SwiperSlide
+            key={song?.key}
+            style={{width: '25%', height: 'auto'}}
+            className="shadow-lg rounded-full animate-slideright"
+            >
+              <Link to={`/artists/${song?.artists[0].adamid}`}>
+                <img src={song?.images.background} alt="name"
+                className="rounded-full" w-full object-cover/>
+                </Link>
+            </SwiperSlide>
+          ))}
+
+        </Swiper>
     
 
 
