8dfb701d71ae57a59c2b1fcccd44cc86012a6945
MikhaelVinicius <mickaelsoel@gmail.com>
2023-02-15 15:22:25 -0300

    implementado top artistas

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,40 @@
-const TopArtists = () => <div>TopArtists</div>;
+import {ArtistCard, Loader, Error} from '../components';
+
+import { useGetArtistDetailsQuery} from "../redux/services/shazamCore";
+
+
+const TopArtists = () => {
+
+const {data, isFetching, error} =  useGetArtistDetailsQuery();
+
+if (isFetching) return <Loader title={"Carregando os artistas em alta..."}/>
+
+if(error) return <Error/>;
+
+return(
+    <div>
+        <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+          Artistas em alta
+        </h2>
+        <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+            {data?.map((track)=> (
+                <ArtistCard key={track.key} track={track}/>
+            ))}
+
+        </div>
+
+
+
+
+    </div>
+    
+
+
+)
+
+
+
+
+}
 
 export default TopArtists;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -16,7 +16,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
         getTopCharts: builder.query({query: () => '/charts/world'}),
         getSongDetails: builder.query({query:(songid) => `/tracks/details?track_id=${songid}` }),
         getSongRelated: builder.query({query: ({songid}) => `/tracks/related?track_id=${songid}` }),
-        getArtistDetails: builder.query({query: ({artistId}) => `/v2/artists/details?artist_id=${(artistId)}` }),
+        getArtistDetails: builder.query({query: ({artistId}) => `/artists/details?artist_id=${(artistId)}` }),
     })
     
 
